d54b7b6bc9fc085174e0140bf6ff38d6f7e44681
SevaStopAll <Sevastopallrh@yandex.ru>
2023-04-24 07:35:29 +0400

    Because kitchen workers see an order by another way, I changed initial realization. Next step - make a domain model for it (and, further, to db).

--- src/main/java/ru/job4j/kitchen/service/SimpleKitchenService.java
+++ src/main/java/ru/job4j/kitchen/service/SimpleKitchenService.java
@@ -3,14 +3,15 @@ package ru.job4j.kitchen.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import ru.job4j.kitchen.domain.Order;
+
+import java.util.Map;
 
 @Service
 @Slf4j
 public class SimpleKitchenService  {
 
     @KafkaListener(topics = "job4j_orders")
-    public void receiveOrder(Order order) {
-        log.debug(order.getStatus().toString());
+    public void receiveOrder(Map order) {
+        log.debug(order.get("dishes").toString());
     }
 }
